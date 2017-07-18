b4c24ee68ee35ccb3264c4c115576a46dd427cd6
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-07-18 22:28:21 +0000

    Initial commit

---
+++ cars.js
@@ -0,0 +1,45 @@
+// 3 car models: sedan suv sportscars
+// rental price for each
+//available or not
+//total available for each category
+//ability to submit a rental request
+
+
+
+var rentals = [
+   {
+        model: "sedan",
+        price: 75,
+        total: 10
+    },
+    {
+        model: "SUV",
+        price: 175,
+        total: 5  
+    },
+    {
+        model: "sports car",
+        price: 125,
+        total: 0
+    }
+    
+    
+];
+    
+function availability() {
+    for (var i = 0; i < rentals.length; i++ ) {
+        if (rentals[i].total > 0) {
+            console.log("The " + rentals[i].model + " is available.");
+        } else {
+            console.log("The " + rentals[i].model + " is unavailable");
+        }
+    }
+}
+
+availability();
+
+
+ 
+ 
+    
+
